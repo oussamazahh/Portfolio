26915d2f7c8b31933b03457615b38bde5c6f35d5
oussamazahh <ssmzahidi@gmail.com>
2023-06-06 20:42:26 +0100

    UPDATE

--- app.js
+++ app.js
@@ -11,3 +11,15 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+$(function() {
+    $('#WAButton').floatingWhatsApp({
+      phone: '+212 7 75 071123', //WhatsApp Business phone number International format-
+      //Get it with Toky at https://toky.co/en/features/whatsapp.
+      headerTitle: 'Comment je peux vous aidez ?', //Popup Title
+      showPopup: true, //Enables popup display
+      buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
+      //headerColor: 'crimson', //Custom header color
+      //backgroundColor: 'crimson', //Custom background button color
+      position: "right"    
+    });
+  });
